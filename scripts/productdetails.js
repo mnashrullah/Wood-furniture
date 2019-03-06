b58cffef8409596ab8cfc4dc8973c841a0d61ef6
mnashrullah <muhammadnashrullah.it@gmail.com>
2019-03-06 11:41:52 +0700

    update productdetails.js

---
+++ scripts/productdetails.js
@@ -0,0 +1,11 @@
+// alert("It works");
+
+function openImg(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+   // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+  }
+ 
